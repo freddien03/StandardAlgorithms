0e81415494a47ced3a32f7835a7536ab40e47f85
freddie <freddienunn@gmail.com>
2020-11-11 11:03:10 +0000

    initial checkin of project

---
+++ StandardAlgorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StandardAlgorithms
+//
+//  Created by Nunn, Frederick (IRG) on 11/11/2020.
+//
+
+import Foundation
+
+print("Hello, World!")
+let a = 5
+print("the value of a is: \(a)")
