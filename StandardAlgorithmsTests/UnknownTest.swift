f2c5edae473c96e351b7a1107ae7266dcdf73ce2
freddie <freddienunn@gmail.com>
2020-11-18 11:18:20 +0000

    finished first+last sum and started second smallest

--- StandardAlgorithmsTests/UnknownTest.swift
+++ StandardAlgorithmsTests/UnknownTest.swift
@@ -19,5 +19,23 @@ class UnknownTest: XCTestCase {
         //assert
         XCTAssertEqual(expected, result)
     }
+    
+    func testReturnsSumOfFirstAndLastFromVariousIntegerArrays() {
+        //arrange
+        let unknown = Unknown()
+        let testData = [(data: [3, 7, 4, 2, 6, 2], expected: 5), (data: [5, 4, 2, 6, 21], expected: 26), (data: [2], expected: 4), (data: [], expected: 0)]
+        //act
+        //assert
+        for test in testData{
+            let result = unknown.sumFirstLast(data: test.data)
+            XCTAssertEqual(result, test.expected)
+        }
+    }
+    
+    func testReturnsSecondSmallestInVariousIntegerArrays() {
+        //arrange
+        //act
+        //assert
+    }
 
 }
